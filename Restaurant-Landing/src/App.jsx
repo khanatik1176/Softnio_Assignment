a233634b05a1c268c3f07e63a8936ea4b5ed91a6
Khan Atik Faisal <khanatik1176@gmail.com>
2024-11-03 22:41:31 +0600

    Topnav_Hero_Section_Done

---
+++ Restaurant-Landing/src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+import Toppart from './Components/Toppart'
+import AboutSection from './Components/AboutSection';
+
+function App() {
+
+  return (
+    <div className="landing-page-area bg-black h-screen">
+      <Toppart/>
+      <AboutSection/>
+    </div>
+
+  );
+}
+
+export default App
---
+++ Restaurant-Landing/src/Components/AboutSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <div className='bg-white h-screen'>
+      
+    </div>
+  )
+}
+
+export default AboutSection
